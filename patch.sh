58bb69a8853460fe9d3f67680ef0daa5ef30406a
Zoe <duplexsys@protonmail.com>
2021-08-06 22:58:28 -0500

    use github

--- patch.sh
+++ patch.sh
@@ -1,7 +1,7 @@
 # remove any existing linux repo
 rm -rf  ./linux
-#clone repo from offical torvalds repo. bypasses github. uses set version and patchlevel for script code and less bugs from patches failing to apply and or not working correctly with new versions
-git clone --branch v5.13 --single-branch https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+#clone repo from github. was using torvalds offical but it was too slow & this is already hosted on github anyways (if that changes I'm switching back to https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git) uses set version and patchlevel for script code and less bugs from patches failing to apply and or not working correctly with new versions
+git clone --branch v5.13 --single-branch https://github.com/torvalds/linux.git
 #enter newly cloned linux repo
 cd linux
 # apply patches from main git repo in numerical order
